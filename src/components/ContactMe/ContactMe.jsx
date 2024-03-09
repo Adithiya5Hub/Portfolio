e54fb585f5713d829dc20bc8afc913605d623cdf
S. Adithiya <adithiya1805@gmail.com>
2024-03-09 16:26:57 +0530

    Contact-Me added-titles

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -8,21 +8,21 @@ const ContactMe = () => {
     <h5>Contact Me</h5>
     <div className="contact-content">
         <div style={{display: "flex"}}>
-           <a href="https://github.com/Adithiya5Hub" style={{padding: "50px"}}> 
+           <a href="https://github.com/Adithiya5Hub" title="GitHub" style={{padding: "50px"}}> 
               <ContactInfo 
                     iconUrl = 'https://cdn-icons-png.flaticon.com/128/2504/2504911.png'
                     
                     
                 />
             </a> 
-           <a href="https://www.linkedin.com/in/s-adithiya-9a252023a/" style={{padding: "50px"}}> 
+           <a href="https://www.linkedin.com/in/s-adithiya-9a252023a/" title="LinkedIn" style={{padding: "50px"}}> 
               <ContactInfo 
                     iconUrl = 'https://cdn-icons-png.flaticon.com/128/15047/15047553.png'
                     
                     
                 />
             </a> 
-           <a href="https://twitter.com/Adithiya1S?t=w37s0kAs5B0NuV41p6khjw&s=08" style={{padding: "50px"}}> 
+           <a href="https://twitter.com/Adithiya1S?t=w37s0kAs5B0NuV41p6khjw&s=08" title="Twitter" style={{padding: "50px"}}> 
               <ContactInfo 
                     iconUrl = 'https://cdn-icons-png.flaticon.com/128/5969/5969020.png'
                     
